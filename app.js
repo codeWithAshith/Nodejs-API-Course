cb986e81431e455391e1341ee4f1ed3ef75a2942
codewithashith <codewithashith@gmail.com>
2022-12-24 19:28:40 +0530

    4. Body parser and cors

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
-// Install morgan for logging
-// npm i morgan
+// npm i --save body-parser
+// to parse url encoded data - to support simple data --> extended: false
+app.use(bodyParser.urlencoded({ extended: false }));
+// to parse json data
+app.use(bodyParser.json());
+
+// cors - cross origin resource sharing
+// if data is from same server - request will succedd
+// for restful api - this will be different
+// for restful api - we have to allow cors
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+  });
+
 const morgan = require("morgan");
 
 const productRoutes = require("./routes/products");
@@ -13,16 +35,12 @@ app.use(morgan("dev"));
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
 
-// middleware for error handling
-// to handle 404 - not found errors
-// sending customer error message
 app.use((req, res, next) => {
   const error = new Error("Not Found");
   error.status = 404;
   next(error);
 });
 
-// middle for handling all errors and sending formatted response
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
   res.json({
