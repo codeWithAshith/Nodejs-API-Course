37f6e11acaea6c0f71cd8f2e7ff63a57b4a5d063
codewithashith <codewithashith@gmail.com>
2022-12-24 18:50:55 +0530

    3. Error handling

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const app = express();
 
-const productRoutes = require("./routes/products"); 
+// Install morgan for logging
+// npm i morgan
+const morgan = require("morgan");
+
+const productRoutes = require("./routes/products");
 const orderRoutes = require("./routes/orders");
+
+app.use(morgan("dev"));
 
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+
+// middleware for error handling
+// to handle 404 - not found errors
+// sending customer error message
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+// middle for handling all errors and sending formatted response
+app.use((error, req, res, error) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 app.listen(3000);
 
