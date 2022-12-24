19989d654c1acdb219a0ee6ea0b53340368b1821
codewithashith <codewithashith@gmail.com>
2022-12-24 20:08:18 +0530

    5. Connected product to mongo db

--- app.js
+++ app.js
@@ -1,39 +1,45 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-
-// npm i --save body-parser
-// to parse url encoded data - to support simple data --> extended: false
-app.use(bodyParser.urlencoded({ extended: false }));
-// to parse json data
-app.use(bodyParser.json());
-
-// cors - cross origin resource sharing
-// if data is from same server - request will succedd
-// for restful api - this will be different
-// for restful api - we have to allow cors
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
-    next();
-  });
-
 const morgan = require("morgan");
+
+// install mongoose - npm i mongoose
+const mongoose = require("mongoose");
 
 const productRoutes = require("./routes/products");
 const orderRoutes = require("./routes/orders");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const mongodb =
+  "mongodb+srv://root:root@cluster0.bypwb3z.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(mongodb, () => {
+    console.log("Connected to MongoDB");
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err.reason));
 
 app.use(morgan("dev"));
 
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use((req, res, next) => {
   const error = new Error("Not Found");
@@ -49,7 +55,5 @@ app.use((error, req, res, next) => {
     },
   });
 });
-
-app.listen(3000);
 
 app.get("/products", productRoutes);
